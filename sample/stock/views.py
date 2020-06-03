d0643e8163693e886cac3e8075e3599e24a4b460
jay_g <jay_g@kr.accommate.com>
2020-06-03 11:58:14 +0900

    2020 06 03 view

--- sample/stock/views.py
+++ sample/stock/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Items
 from .mongo_db import MongoDB
@@ -8,23 +8,24 @@ from .mongo_db import MongoDB
 
 class StockItemList(LoginRequiredMixin, ListView):
     model = Items
-    context_object_name = 'stock_items'
+    paginate_by = 10
     template_name = 'stock/stock_item_list.html'
+    context_object_name = 'stock_items'
     extra_context = {
         'view_title': 'Stock List'
     }
 
 
-class StockItemDetail(LoginRequiredMixin, ListView):
-    model = Items
+class StockItemDetail(LoginRequiredMixin, DetailView):
     template_name = 'stock/stock_item_detail.html'
-    context_object_name = 'stock_items'
+    context_object_name = 'stock_item'
+
+    def get_object(self, queryset = None):
+        return get_object_or_404(Items, code = self.kwargs['code'])
 
     def get_context_data(self, **kwargs):
-        mongo = MongoDB()
+        context = super().get_context_data(**kwargs)
+        context['view_title'] = 'Stock Detail'
+        context['finance_info'] = MongoDB().find_one('finance_info', { "stock_items_code": self.kwargs['code'] })
 
-        return {
-            'view_title'  : 'Stock Detail',
-            'stock_item'  : get_object_or_404(Items, code = self.kwargs['code']),
-            'finance_info': mongo.find_one('finance_info', { "stock_items_code": self.kwargs['code'] })
-        }
+        return context
