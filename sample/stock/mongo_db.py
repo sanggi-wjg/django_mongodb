d7eac9daebbcca66e57895731f055dad2a8af4ef
sanggi-wjg <girr311@naver.com>
2020-06-17 14:37:25 +0900

    Update mongo_db.py

--- sample/stock/mongo_db.py
+++ sample/stock/mongo_db.py
@@ -46,15 +46,19 @@ class MongoDB:
     ##############################################################################################
     # UPDATE
     ##############################################################################################
+    def update(self, collection_name: str, document_id, data, multi = True):
+        document = self.db(collection_name).update(document_id, { "$set": data }, multi = multi)
+        return document.acknowledged
+
     def update_or_create(self, collection_name: str, document_id, data):
-        document = self.db[collection_name].update_one({ "_id": ObjectId(document_id) }, { "$set": data }, upsert = True)
+        document = self.db[collection_name].update_one(document_id, { "$set": data }, upsert = True)
         return document.acknowledged
 
     ##############################################################################################
     # DELETE
     ##############################################################################################
     def remove(self, collection_name: str, document_id):
-        document = self.db[collection_name].delete_one({ "_id": ObjectId(document_id) })
+        document = self.db[collection_name].delete_one(document_id)
         return document.acknowledged
 
     ##############################################################################################
